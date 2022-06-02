5ddce901a1539cb9b8d20bde7e86e985405101b6
Nikhil Garg <79260433+itznikhil@users.noreply.github.com>
2022-06-02 21:17:50 +0530

    Update environment.js

--- backend/environment.js
+++ backend/environment.js
@@ -1,5 +1,5 @@
 export default {
-  oauthCallback: "http://localhost:8080",
+  oauthCallback: "http://54.176.83.139:8080",
   CONSUMER_SECRET: "4HpAyBDGstsC7D8zaXFFXSvibRJVrtFybFsgjDIKf1vfjQJ9G1",
   CONSUMER_KEY: "FgewuYt89IB7lsVVqkmZfCUmJ",
 };
