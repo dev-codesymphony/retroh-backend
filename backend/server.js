c6fa5873448bea92a94f9092d0b49d2e2887a095
nikhil garg <nikhilgargcoder@gmail.com>
2022-07-20 22:42:51 +0530

    Small change

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,19 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/discord", discordRoutes);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
